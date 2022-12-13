9a936f5a02cb6221da48f665087b5b88f3a5a560
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2022-12-13 11:59:59 +0800

    Modify build script to include project names in outfiles

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ select d in */; do test -n "$d" && break; echo ">>> Invalid Selection"; done
 cd "$d"
 
 # Combine all to one file
-OUTPUT_DIR=${SCRIPT_DIR}/main.html
-cat sketch.html > $OUTPUT_DIR
-printf "\n<script>\n" >> $OUTPUT_DIR
-cat sketch.js >> $OUTPUT_DIR
-printf "\n</script>" >> $OUTPUT_DIR
+OUTPUT_DIR=${SCRIPT_DIR}/${d:0:-1}.html
+echo $OUTPUT_DIR
+cat sketch.html > "$OUTPUT_DIR"
+printf "\n<script>\n" >> "$OUTPUT_DIR"
+cat sketch.js >> "$OUTPUT_DIR"
+printf "\n</script>" >> "$OUTPUT_DIR"
